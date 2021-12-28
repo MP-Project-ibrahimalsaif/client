6d53b3780f600456ce5ed542d91e3fa5f436ad9a
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-28 10:10:41 +0300

    profile page v1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Explore from "./components/Explore";
 import Auction from "./components/Auction";
+import Profile from "./components/Profile";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import CreateAuction from "./components/CreateAuction";
@@ -16,6 +17,7 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/explore" element={<Explore />} />
         <Route exact path="/explore/:id" element={<Auction />} />
+        <Route exact path="/users/:id" element={<Profile />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/create_auction" element={<CreateAuction />} />
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import Navbar from "./../Navbar";
+import Card from "./../Card";
+import Footer from "./../Footer";
 import "./style.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { id } = useParams();
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    getUserAuction();
+    // eslint-disable-next-line
+  }, []);
+
+  const getUserAuction = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/users/${id}`
+      );
+      setProfile(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Navbar show={true} />
+      <div className="profile">
+        {profile ? (
+          <>
+            <div className="profileInfo">
+              <img src={profile.avatar} alt={`${profile.name} avatar`} />
+              <h1>{profile.name}</h1>
+            </div>
+            <div className="profileTitle">
+              <h1>{profile.name} Auctions</h1>
+            </div>
+            <div className="profileCards">
+              {profile.auctions.map((auction) => {
+                return (
+                  <Card preview={false} data={auction} key={auction._id} />
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <div className="center">
+            <div className="lds-ring">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Profile;
